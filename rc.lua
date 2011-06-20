66a2790b8ce18cd94eeab997184d58c2b1dcc46b
Kevin Daudt <compufreak@gmail.com>
2011-06-20 11:15:25 +0200

    Changed some shortcuts regarding window management

--- rc.lua
+++ rc.lua
@@ -31,6 +31,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
+    awful.layout.suit.max,
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
@@ -38,7 +39,6 @@ layouts =
     awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
-    awful.layout.suit.max,
     awful.layout.suit.magnifier
 }
 -- }}}
@@ -48,7 +48,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[8])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
 end
 -- }}}
 
@@ -160,7 +160,7 @@ for s = 1, screen.count() do
         },
         mylayoutbox[s],
         mytextclock,
-        s == screen.count() and mysystray or nil,
+        s == 2 and mysystray or nil,
         batwidget,
         cpuwidget,
         mytasklist[s],
@@ -196,10 +196,8 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "w", function () mymainmenu:show(true)        end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
-    awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
-    awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+    awful.key({ modkey, "Control" }, "j", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Control" }, "k", function () awful.client.swap.byidx( -1)    end),
     awful.key({ modkey, "Shift"   }, "j", function () awful.screen.focus_relative( 1) end),
     awful.key({ modkey, "Shift"   }, "k", function () awful.screen.focus_relative(-1) end),
     awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
